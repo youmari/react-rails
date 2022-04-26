d01f1849b6038ede9160ca11ef971b7712d4b110
youmari <youmari300@gmail.com>
2022-04-26 17:17:01 +0000

    Add new component

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'static/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "static#index"
+end
