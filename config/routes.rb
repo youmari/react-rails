eff7e4b5c4038cdf922da0e0cdc8a59270ae3308
youmari <youmari300@gmail.com>
2022-04-27 06:34:57 +0000

    Set up routes to get directed to react router

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
   # Defines the root path route ("/")
   root "static#index"
 end
